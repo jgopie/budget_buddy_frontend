f14caa3d4b4afb1f695561d86de9db9579bdf78f
Jordan Ricardo Gopie <jordangopie@live.com>
2025-02-27 18:36:55 -0400

    Add additional project files and configuration for CMake builds across multiple architectures

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,7 +1,6 @@
 import 'package:budget_buddy_frontend/components/account_list.component.dart';
 import 'package:budget_buddy_frontend/components/budget_buddy_appbar.component.dart';
 import 'package:budget_buddy_frontend/providers/user_account_information.provider.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
